bda5b36f91d9dcd62ad9414a30fc1d34b688c5fd
Leia Sefkin <leiasefkin@Leias-Air.(none)>
2020-02-06 22:12:06 -0800

    Moving things around, documentation

--- README.md
+++ README.md
@@ -19,3 +19,25 @@ node normalizer.js input.csv output.csv
 ```
 
 Any errors encountered due to imporperly formatted data will cause the row being processed to be dropped. 
+
+This script expects an input CSV file formatted with the following headers:
+
+```
+Timestamp
+ZIP
+FullName
+Address
+FooDuration
+BarDuration
+Notes
+```
+
+* Timestamp expects an input format of `MM/DD/YY hh:mm:ss A` and will be convereted into ISO-8601, from Pacific Standard to Eastern Standard time. To change the zones update the constants in `rowTransform.js`.
+
+* ZIP will be expected to be 5 digits, where not data will be padded with preceding '0's.
+
+* FullName will be translated to uppercase ALLCAPS
+
+* FooDuration and BarDuraition will be translated into seconds. All formats accepted are following the [moment.duration docs](https://momentjs.com/docs/#/durations/)
+
+* Notes and Address will be left as is, aside from sanitizing input against UTF-8 standards
---
+++ rowTransform.js
@@ -0,0 +1,84 @@
+const moment = require('moment-timezone');
+
+/*
+  Check and attempt to sanitize/normalize Unicode according to XML 1.0 standards
+  Regex borrowed from here: 
+    https://www.ryadel.com/en/javascript-remove-xml-invalid-chars-characters-string-utf8-unicode-regex/
+*/
+function normalizeUnicode(input) {
+  const regex = /([^\x09\x0A\x0D\x20-\uD7FF\uE000-\uFFFC\u{10000}-\u{10FFFF}])/ug;
+  return input.replace(regex, '\uFFFD');
+}
+
+//TimeZone definitions for converting timestamps
+const inputTimeZone = 'America/Los_Angeles';
+const outputTimeZone = 'America/New_York';
+
+/*
+  Normalize the data for each row, all params listed are required
+
+  @param inputRow {Object} - the input row of data to be processed
+  @param inputRow.Timestamp {String} - Date string formatted as MM/DD/YY hh:mm:ss A 
+  @param inputRow.ZIP {String} - Zip code 
+  @param inputRow.FullName {String} - Full Name
+  @param inputRow.Address {String} - Address string
+  @param inputRow.FooDuration {String} - Duration 
+    (accepts all formats of the moment.duration method: https://momentjs.com/docs/#/durations/)
+  @param inputRow.BarDuration {String} - Duration
+    (same as above)
+  @param inputRow.Notes {String} - Text notes
+*/
+function rowTransform(inputRow, cb) {
+  let outputRow = {};
+
+  //Convert the Timestamp value to ISO_8601 format
+  let date = moment.tz(inputRow.Timestamp, 'MM/DD/YY hh:mm:ss a', inputTimeZone);
+
+  //Ensure we got a valid date
+  if(date.isValid()) {
+    //Convert timezone and save to Output
+    outputRow.Timestamp = date.tz(outputTimeZone);
+  } else {
+    return cb(new Error(`Invalid Date: ${timestamp}`));
+  }
+
+  //Zip Code -> prefill with 0 if less than 5 digits
+  let zipCode = inputRow.ZIP;
+  if(zipCode.length < 5) {
+    zipCode = zipCode.padStart(5, '0');
+  }
+
+  outputRow.ZIP = zipCode;
+
+  //FullName -> convert to uppercase
+  let name = inputRow.FullName.toUpperCase();
+  outputRow.FullName = name;
+
+  //Address -> pass through as is aside from unicode validation
+  outputRow.Address = normalizeUnicode(inputRow.Address);
+
+  //FooDuration and BarDuration -> convert from HH:MM:SS.MS to total number of seconds expressed
+  let fooDuration = moment.duration(inputRow.FooDuration);
+  if(fooDuration.isValid()) {
+    outputRow.FooDuration = fooDuration.asSeconds();
+  } else {
+    return cb(new Error('Error parsing FooDuration seconds'));
+  }
+
+  let barDuration = moment.duration(inputRow.BarDuration);
+  if(barDuration.isValid()) {
+    outputRow.BarDuration = barDuration.asSeconds();
+  } else {
+    return cb(new Error('Error parsing BarDuration seconds'));
+  }
+
+  //Total duration set to be total of Foo + Bar duration
+  outputRow.TotalDuration = outputRow.FooDuration + outputRow.BarDuration;
+
+  //Save notes sanitized 
+  outputRow.Notes = normalizeUnicode(inputRow.Notes);
+
+  cb(null, outputRow);
+}
+
+module.exports = rowTransform;
